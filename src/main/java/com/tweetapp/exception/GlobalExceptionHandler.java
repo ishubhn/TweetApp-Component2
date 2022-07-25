3b4c226b9cc6876aaa7020fadbfb4b680ed03391
Shubham Nagre <Shubham.Nagre@Cognizant.com>
2022-07-25 22:11:31 +0530

    Added User Already Exist Exception (CONFLICT)

--- src/main/java/com/tweetapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tweetapp/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.tweetapp.exception;
 
+import org.apache.tomcat.jni.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +19,14 @@ public class GlobalExceptionHandler {
 				request.getDescription(false));
 
 		return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+	}
+
+	public ResponseEntity<?> handleUserAlreadyExistException
+			(UserAlreadyExistException exception, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
 	}
 
 	// Global Exceptions
---
+++ src/main/java/com/tweetapp/exception/UserAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.tweetapp.exception;
+
+public class UserAlreadyExistException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public UserAlreadyExistException(String message) {
+		super(message);
+	}
+}
