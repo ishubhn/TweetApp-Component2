b034bd3be88ff595d8f0cbc9f80f1147f62c7f16
Shubham Nagre <Shubham.Nagre@Cognizant.com>
2022-07-28 12:20:58 +0530

    Added blank tweet controller

--- src/main/java/com/tweetapp/model/TweetEntity.java
+++ src/main/java/com/tweetapp/model/TweetEntity.java
@@ -5,6 +5,7 @@ import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
@@ -14,6 +15,7 @@ import java.util.Date;
 public class TweetEntity {
 	@Id
 	@GeneratedValue
+	@NotNull
 	private long id;
 
 	@ManyToOne(targetEntity = UserEntity.class)
