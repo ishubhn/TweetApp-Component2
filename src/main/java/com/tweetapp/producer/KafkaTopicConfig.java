3b239d615334817cfc472152a38b8f4a4e0e6152
Shubham Nagre <shubham.nagre@cognizant.com>
2022-08-19 19:28:12 +0530

    Added Kafka dependency

---
+++ src/main/java/com/tweetapp/producer/KafkaTopicConfig.java
@@ -0,0 +1,20 @@
+package com.tweetapp.producer;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+	private static final String TOPIC = "tweet-events";
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder
+				.name(TOPIC)
+				.replicas(1)
+				.build();
+	}
+}
---
+++ src/main/java/com/tweetapp/producer/TweetProducer.java
@@ -0,0 +1,26 @@
+package com.tweetapp.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TweetProducer {
+	private static final Logger LOG = LoggerFactory.getLogger(TweetProducer.class);
+	private static final String TOPIC = "tweet-events";
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public TweetProducer(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+	}
+
+	public void sendMessage(String msg) {
+		LOG.info("Message sent -> " + msg);
+		this.kafkaTemplate.send(TOPIC, msg);
+
+	}
+}
