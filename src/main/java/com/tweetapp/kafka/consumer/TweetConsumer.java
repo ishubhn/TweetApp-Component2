206659df54e79b68d27b80abe1e794a1024b7aa1
ishubhn <imshubhn@gmail.com>
2022-08-23 11:03:13 +0530

    Added kafka consumer configurations

---
+++ src/main/java/com/tweetapp/kafka/consumer/TweetConsumer.java
@@ -0,0 +1,17 @@
+package com.tweetapp.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class TweetConsumer {
+
+    @KafkaListener(topics = "tweet-events", groupId = "gteroup_id")
+    public void consume(String message) {
+        log.info("message -> {}", message);
+        System.out.println("message = " + message);
+    }
+
+}
